e272879c088898499424d9a933afa9a1b84f1e4e
anildadhich0024 <anildadhich.raja@gmail.com>
2021-10-11 09:36:07 +0530

    JS Change

--- post.php
+++ post.php
@@ -8,20 +8,8 @@ if ($con -> connect_errno) {
   exit();
 } 
 
-$time = explode(' : ',base64_decode($_POST['last_click_time']));
-$last_second = abs(20 - $time[0]) - 1;
-$last_mili_second = abs(60 - $time[1]);
-
-
-if($last_second == 0 && $last_mili_second == 0) {
-  $last_second = 20;
-  $last_mili_second = 0;
-
-}
-
-$last_click_time = str_pad($last_second, 2, '0', STR_PAD_LEFT).' : '.str_pad($last_mili_second, 2, '0', STR_PAD_LEFT);
 $sql = "INSERT INTO user_submissions (full_name, email_address, final_score, pod_name, last_click_time)
-VALUES ('".$_POST['full_name']."', '".$_POST['email_address']."', '".base64_decode($_POST['final_score'])."', '".base64_decode($_POST['pod_name'])."', '".$last_click_time."')";
+VALUES ('".$_POST['full_name']."', '".$_POST['email_address']."', '".base64_decode($_POST['final_score'])."', '".base64_decode($_POST['pod_name'])."', '".base64_decode($_POST['last_click_time'])."')";
 
 if (mysqli_query($con, $sql)) {
   echo '<script>window.location.href= "result.php"</script>';
