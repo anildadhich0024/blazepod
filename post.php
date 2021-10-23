751084069f7c953a620ac391906c720c3b7dea2e
anildadhich0024 <anildadhich.raja@gmail.com>
2021-10-23 13:26:51 +0530

    JS Change

--- post.php
+++ post.php
@@ -8,13 +8,19 @@ if ($con -> connect_errno) {
   exit();
 } 
 
-$sql = "INSERT INTO user_submissions (full_name, email_address, final_score, pod_name, last_click_time)
-VALUES ('".$_POST['full_name']."', '".$_POST['email_address']."', '".base64_decode($_POST['final_score'])."', '".base64_decode($_POST['pod_name'])."', '".base64_decode($_POST['last_click_time'])."')";
-
-if (mysqli_query($con, $sql)) {
-  echo '<script>window.location.href= "index.php"</script>';
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($con);
-} 
+$record = mysqli_fetch_array(mysqli_query($con, "SELECT * FROM user_submissions WHERE email_address = '".$_POST['email_address']."' AND pod_name = '".base64_decode($_POST['pod_name'])."'"));
+if(empty($record))
+{
+  $sql = mysqli_query($con, "INSERT INTO user_submissions (full_name, email_address, final_score, pod_name, last_click_time)
+  VALUES ('".$_POST['full_name']."', '".$_POST['email_address']."', '".base64_decode($_POST['final_score'])."', '".base64_decode($_POST['pod_name'])."', '".base64_decode($_POST['last_click_time'])."')");
+}
+else
+{
+  if(base64_decode($_POST['final_score']) > $record['final_score'])
+  {
+    $sql = mysqli_query($con, "UPDATE user_submissions set final_score = '".base64_decode($_POST['final_score'])."', last_click_time = '".base64_decode($_POST['last_click_time'])."' WHERE user_id = '".$record['user_id']."'");
+  }
+}
+echo '<script>window.location.href= "index.php"</script>';
 
 ?>
