047105e9474752b7dd8841c0ced7f46eb146de25
anildadhich0024 <anildadhich.raja@gmail.com>
2021-11-08 09:58:08 +0530

    Final Code

--- post.php
+++ post.php
@@ -11,10 +11,11 @@ if ($con -> connect_errno) {
 $code_array = array( "1"=>"BF15", "2"=>"CHASE20", "3"=>"GAME25");
 $code_key = array_rand($code_array);
 $code_name = $code_array[$code_key];
-
+$send_email = false;
 $record = mysqli_fetch_array(mysqli_query($con, "SELECT * FROM user_submissions WHERE email_address = '".$_POST['email_address']."' AND pod_name = '".base64_decode($_POST['pod_name'])."'"));
 if(empty($record))
 {
+  $send_email = true;
   $sql = mysqli_query($con, "INSERT INTO user_submissions (full_name, email_address, total_points, positive_points, neg_points, final_score, pod_name, last_click_time, accept_condition, code_name)
   VALUES ('".$_POST['full_name']."', '".$_POST['email_address']."', '".base64_decode($_POST['total_points'])."', '".base64_decode($_POST['positive_points'])."', '".base64_decode($_POST['neg_points'])."', '".base64_decode($_POST['final_point'])."', '".base64_decode($_POST['pod_name'])."', '".base64_decode($_POST['last_click_time'])."', '".$_POST['accept_condition']."', '".$code_name."')");
 }
@@ -22,12 +23,14 @@ else
 {
   if(base64_decode($_POST['final_point']) > $record['final_score'])
   {
+    $send_email = true;
     $sql = mysqli_query($con, "UPDATE user_submissions set total_points = '".base64_decode($_POST['total_points'])."', positive_points = '".base64_decode($_POST['positive_points'])."', neg_points = '".base64_decode($_POST['neg_points'])."', final_score = '".base64_decode($_POST['final_point'])."', last_click_time = '".base64_decode($_POST['last_click_time'])."', code_name = '".$code_name."' WHERE user_id = '".$record['user_id']."', '".$_POST['accept_condition']."'");
   }
   if(base64_decode($_POST['final_point']) == $record['final_score'])
   {
     if(strtotime(base64_decode($_POST['last_click_time'])) < strtotime($record['last_click_time']))
     {
+      $send_email = true;
       $sql = mysqli_query($con, "UPDATE user_submissions set total_points = '".base64_decode($_POST['total_points'])."', positive_points = '".base64_decode($_POST['positive_points'])."', neg_points = '".base64_decode($_POST['neg_points'])."', final_score = '".base64_decode($_POST['final_point'])."', last_click_time = '".base64_decode($_POST['last_click_time'])."', code_name = '".$code_name."' WHERE user_id = '".$record['user_id']."', '".$_POST['accept_condition']."'");
     }
   }
@@ -35,31 +38,33 @@ else
 $_SESSION["pod_name"] = $_POST['pod_name'];
 $_SESSION["code_name"] = base64_encode($code_name);
 
-$fields_string = '';
-$url = 'https://mail.topodiumgroup.com/rest/automation/trigger_workflow';
-$fields = array(
-    'api_id'                 => 'MoHx_J7cKxV4M5tyaKoM205-9cs.',
-    'api_key'                => '7gdcavfGsMwRvf6WuohNMNS4RrocCVcSnwXejcXM8mI.',
-    'workflow_api_identifier' => urlencode('BPod/BF2021/trigger_workflow'),
-    'email_address'           => urlencode($_POST['email_address']),
-    'first_name'              => urlencode($_POST['full_name']),
-    'variable_data_1'         => urlencode($code_name),
-);
+if($send_email)
+{
+  $fields_string = '';
+  $url = 'https://mail.topodiumgroup.com/rest/automation/trigger_workflow';
+  $fields = array(
+      'api_id'                 => 'MoHx_J7cKxV4M5tyaKoM205-9cs.',
+      'api_key'                => '7gdcavfGsMwRvf6WuohNMNS4RrocCVcSnwXejcXM8mI.',
+      'workflow_api_identifier' => urlencode('BPod/BF2021/trigger_workflow'),
+      'email_address'           => urlencode($_POST['email_address']),
+      'first_name'              => urlencode($_POST['full_name']),
+      'variable_data_1'         => urlencode($code_name),
+  );
 
-//url-ify the data for the POST
-foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-rtrim($fields_string, '&');
+  //url-ify the data for the POST
+  foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+  rtrim($fields_string, '&');
 
-//open connection
-$ch = curl_init();
+  //open connection
+  $ch = curl_init();
 
-//set the url, number of POST vars, POST data
-curl_setopt($ch,CURLOPT_URL, $url);
-curl_setopt($ch,CURLOPT_POST, count($fields));
-curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-curl_exec($ch);
-curl_close($ch);
-
+  //set the url, number of POST vars, POST data
+  curl_setopt($ch,CURLOPT_URL, $url);
+  curl_setopt($ch,CURLOPT_POST, count($fields));
+  curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+  curl_exec($ch);
+  curl_close($ch);
+}
 echo '<script>window.location.href= "result"</script>';
 
 ?>
